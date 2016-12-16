c8fcb488e70b4aec3d851507098f95e43e90da8d
Joon <joon.durandt@gmail.com>
2016-12-16 08:00:40 +0000

    Added animation for the LED screen of Puzzle 8b

--- AdventOfCodeCSharp/AdventOfCodeCSharp/Puzzle8.cs
+++ AdventOfCodeCSharp/AdventOfCodeCSharp/Puzzle8.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AdventOfCodeCSharp
@@ -13,21 +14,30 @@ namespace AdventOfCodeCSharp
         {
             bool[,] screen = new bool[50, 6];
 
+            int startPosition = Console.CursorTop;
+            Console.CursorVisible = false;
+
             foreach (string s in input.Split(Environment.NewLine.ToCharArray(),
                 StringSplitOptions.RemoveEmptyEntries))
             {
                 BaseCommand bc = CommandFactory.CreateCommand(s);
                 bc.ApplyCommand(screen);
-            }
-       
-            for (int y = 0; y < 6; y++)
-            {
-                for (int x = 0; x < 50; x++)
+                for (int y = 0; y < 6; y++)
                 {
-                    Console.Write(screen[x, y] ? '#' : ' ');
+                    for (int x = 0; x < 50; x++)
+                    {
+                        Console.CursorLeft = x;
+                        Console.CursorTop = startPosition + y;
+                        Console.Write(screen[x, y] ? '#' : ' ');
+                    }
                 }
-                Console.WriteLine();
             }
+
+            Console.CursorTop = startPosition + 7;
+            Console.CursorLeft = 0;
+
+            Console.CursorVisible = true;
+
             return "read it!!";
         }
 
